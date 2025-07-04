ada4c005ea77cd8b0aa3cff5f36bbfe33be8b287
t1end4t <tiendatarse3004@gmail.com>
2025-07-04 08:56:26 +0700

    not calibre, too much time

--- nixos/base/apps/default.nix
+++ nixos/base/apps/default.nix
@@ -7,9 +7,7 @@
     ./opengl.nix
     ./thunar.nix
     ./virt-manager.nix
-    ./virtual-box.nix
     ./nix-ld.nix
     ./flatpak.nix
-    ./calibre.nix
   ];
 }
--- nixos/base/apps/virtual-box.nix
+++
@@ -1,5 +0,0 @@
-{
-  # source: https://nixos.wiki/wiki/VirtualBox
-  # virtualisation.virtualbox.host.enable = true;
-  # users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
-}
