eec17b228e304011bb74abef36fa1abe370b34a9
t1end4t <tiendatarse3004@gmail.com>
2025-07-04 12:25:27 +0700

    add obs

--- home-manager/desktop-apps/office.nix
+++ home-manager/desktop-apps/office.nix
@@ -5,4 +5,13 @@
     koodo-reader # epub reader
     libreoffice-qt # office tools
   ];
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      # obs-backgroundremoval
+      # obs-pipewire-audio-capture
+    ];
+  };
 }
