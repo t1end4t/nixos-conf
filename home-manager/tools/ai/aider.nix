13c9cba0854ed4371e83083b3c8fb70504dc281e
t1end4t <tiendatarse3004@gmail.com>
2025-07-18 18:49:12 +0700

    not use aider for web

--- home-manager/tools/ai/aider.nix
+++ home-manager/tools/ai/aider.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    unstable.aider-chat-with-playwright # best programming tools
-    playwright-driver.browsers # for using aider
-    playwright
+    unstable.aider-chat # best programming tools
+    # unstable.aider-chat-with-playwright # best programming tools
+    # playwright-driver.browsers # for using aider
+    # playwright
   ];
 }
