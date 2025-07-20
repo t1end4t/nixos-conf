04cde119773a518f151186d5e2f47425d4a0a416
t1end4t <tiendatarse3004@gmail.com>
2025-07-20 17:12:06 +0700

    use stable version because break

--- home-manager/tools/ai/aider.nix
+++ home-manager/tools/ai/aider.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    unstable.aider-chat # best programming tools
     # unstable.aider-chat-with-playwright # best programming tools
-    # playwright-driver.browsers # for using aider
-    # playwright
+    aider-chat-with-playwright # best programming tools
+    playwright-driver.browsers # for using aider
+    playwright
   ];
 }
