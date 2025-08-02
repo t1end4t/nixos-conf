2fc1664023b2297dbb4633593aec08940c4d18be
t1end4t <tiendatarse3004@gmail.com>
2025-08-02 21:12:57 +0700

    try to test stylix

--- nixos/base/stylix.nix
+++ nixos/base/stylix.nix
@@ -1,3 +1,7 @@
+{ pkgs, ... }:
 {
   stylix.enable = true;
+  stylix.targets.gnome.enable = false;
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+  stylix.image = ./aesthetic_deer.jpg;
 }
