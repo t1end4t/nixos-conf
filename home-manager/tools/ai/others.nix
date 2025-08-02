14d1b72cae872bb2076fa5d8230d57e87adaaca1
t1end4t <tiendatarse3004@gmail.com>
2025-08-02 12:53:25 +0700

    use lastest gemini instead

--- home-manager/tools/ai/others.nix
+++ home-manager/tools/ai/others.nix
@@ -2,6 +2,9 @@
 
 {
   home.packages = with pkgs; [
-    unstable.gemini-cli # to work with gemini
+    # unstable.gemini-cli # to work with gemini
+
+    # npm set prefix ~/.npm-global
+    nodejs
   ];
 }
