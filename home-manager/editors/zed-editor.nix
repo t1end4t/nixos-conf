78581e7f8e17793737a3e4ea0945fd0b46f97562
t1end4t <tiendatarse3004@gmail.com>
2025-09-05 17:30:20 +0700

    update

--- home-manager/editors/zed-editor.nix
+++ home-manager/editors/zed-editor.nix
@@ -1,31 +1,36 @@
 { pkgs, ... }:
 {
+  # for login and remember key
+  services.gnome-keyring.enable = true;
+
+  # Provides org.gnome.keyring.SystemPrompter
+  home.packages = [ pkgs.gcr ];
+
   programs.zed-editor = {
     enable = true;
-    # settings = {
-    #   # Default assistant provider → OpenRouter
-    #   "assistant.default_provider" = "openrouter";
+    package = pkgs.unstable.zed-editor;
+    userSettings = {
+      helix_mode = true;
+      scrollbar = {
+        show = "never";
+      };
 
-    #   # Providers
-    #   "assistant.providers" = {
-    #     openrouter = {
-    #       api_url = "https://openrouter.ai/api/v1";
-    #       # you’ll need to export your API key in env or here if supported
-    #       # api_key = "YOUR_OPENROUTER_API_KEY";
-    #     };
-
-    #     gemini = {
-    #       # In recent Zed versions, Gemini works via the Gemini CLI
-    #       # If it's in PATH, you don't need to set cli_path
-    #       # cli_path = "/home/you/.local/bin/gemini";
-    #     };
-    #   };
-
-    #   # Order of fallback/selection
-    #   "assistant.provider_order" = [
-    #     "openrouter"
-    #     "gemini"
-    #   ];
-    # };
+      languages = {
+        Python = {
+          formatter = {
+            external = {
+              command = "ruff";
+              arguments = [
+                "format"
+                "-"
+                "--line-length"
+                "80"
+              ];
+            };
+          };
+          format_on_save = "on";
+        };
+      };
+    };
   };
 }
