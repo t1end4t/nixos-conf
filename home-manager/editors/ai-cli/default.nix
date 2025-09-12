3b1d6e8e4f5bdf2781c76a74adaf1a668f2e9fb3
t1end4t <tiendatarse3004@gmail.com>
2025-09-12 08:29:12 +0700

    add 2 secrete file

--- home-manager/editors/ai-cli/default.nix
+++ home-manager/editors/ai-cli/default.nix
@@ -15,7 +15,11 @@ in
   ];
 
   # create symlink for secret aider and aider example
-  home.file."aider.conf.yml.gpg" = {
-    source = "${ROOT}/aider.conf.yml.gpg";
+  home.file.".aider.conf.yml.gpg" = {
+    source = "${ROOT}/secrets/aider.conf.yml.gpg";
+  };
+
+  home.file.".claude-code-router/config.json.gpg" = {
+    source = "${ROOT}/secrets/claude-code-router-config.json.gpg";
   };
 }
