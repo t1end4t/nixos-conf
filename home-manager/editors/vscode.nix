1fef2119a74cbb6bed0527199162b6c079c157d3
t1end4t <tiendatarse3004@gmail.com>
2025-09-23 14:48:28 +0700

    update lock file

--- home-manager/editors/vscode.nix
+++ home-manager/editors/vscode.nix
@@ -4,10 +4,10 @@
     enable = true;
     profiles.default = {
       extensions = with pkgs.vscode-extensions; [
-        pkief.material-icon-theme
+        # pkief.material-icon-theme
       ];
       userSettings = {
-        "workbench.iconTheme" = "material-icon-theme";
+        # "workbench.iconTheme" = "material-icon-theme";
         "editor.minimap.enabled" = false;
         "baml.bamlPanelOpen" = true;
       };
