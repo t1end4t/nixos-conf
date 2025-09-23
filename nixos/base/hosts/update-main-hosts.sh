30a297992a5b195d4c6c3461781f25c89718682c
t1end4t <tiendatarse3004@gmail.com>
2025-09-23 14:04:22 +0700

    block youtube

--- nixos/base/hosts/update-main-hosts.sh
+++ nixos/base/hosts/update-main-hosts.sh
@@ -13,42 +13,42 @@ if [ $? -eq 0 ]; then
     echo "Successfully updated $OUTPUT_FILE"
     
     # Comment out Reddit section (starting with "# Reddit" and all following non-comment lines until next comment or EOF)
-    echo "Commenting out Reddit section..."
-    awk '
-    /^# Reddit$/ { 
-        in_reddit_section = 1
-        print "# " $0
-        next
-    }
-    in_reddit_section && /^[0-9]/ { 
-        print "# " $0
-        next
-    }
-    in_reddit_section && /^#/ { 
-        in_reddit_section = 0
-    }
-    { print }
+    # echo "Commenting out Reddit section..."
+    # awk '
+    # /^# Reddit$/ { 
+    #     in_reddit_section = 1
+    #     print "# " $0
+    #     next
+    # }
+    # in_reddit_section && /^[0-9]/ { 
+    #     print "# " $0
+    #     next
+    # }
+    # in_reddit_section && /^#/ { 
+    #     in_reddit_section = 0
+    # }
+    # { print }
     # ' "$OUTPUT_FILE" > "$OUTPUT_FILE.tmp" && mv "$OUTPUT_FILE.tmp" "$OUTPUT_FILE"
     
     # Comment out Twitter section (starting with "# Twitter" and all following non-comment lines until next comment or EOF)
-    echo "Commenting out Twitter section..."
-    awk '
-    /^# Twitter$/ { 
-        in_twitter_section = 1
-        print "# " $0
-        next
-    }
-    in_twitter_section && /^[0-9]/ { 
-        print "# " $0
-        next
-    }
-    in_twitter_section && /^#/ { 
-        in_twitter_section = 0
-    }
-    { print }
-    ' "$OUTPUT_FILE" > "$OUTPUT_FILE.tmp" && mv "$OUTPUT_FILE.tmp" "$OUTPUT_FILE"
+    # echo "Commenting out Twitter section..."
+    # awk '
+    # /^# Twitter$/ { 
+    #     in_twitter_section = 1
+    #     print "# " $0
+    #     next
+    # }
+    # in_twitter_section && /^[0-9]/ { 
+    #     print "# " $0
+    #     next
+    # }
+    # in_twitter_section && /^#/ { 
+    #     in_twitter_section = 0
+    # }
+    # { print }
+    # ' "$OUTPUT_FILE" > "$OUTPUT_FILE.tmp" && mv "$OUTPUT_FILE.tmp" "$OUTPUT_FILE"
     
-    echo "Reddit and Twitter sections commented out"
+    # echo "Reddit and Twitter sections commented out"
 else
     echo "Failed to download the hosts file" >&2
     exit 1
