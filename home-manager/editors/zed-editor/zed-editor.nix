ba5fe1d9c828a2acf8ec08a130b678e258776553
t1end4t <tiendatarse3004@gmail.com>
2025-09-24 13:42:07 +0700

    add keymap for zed

--- home-manager/editors/zed-editor/zed-editor.nix
+++ home-manager/editors/zed-editor/zed-editor.nix
@@ -1,12 +1,49 @@
 { pkgs, ... }:
-# let
-#   ROOT = builtins.toString ./.;
-#   settings_toml = (builtins.readFile "${ROOT}/config.toml");
-#   languages_toml = (builtins.readFile "${ROOT}/languages.toml");
-# in
+let
+  ROOT = builtins.toString ./.;
+  keymap_json = (builtins.readFile "${ROOT}/keymap.json");
+in
 {
   programs.zed-editor = {
     enable = true;
     package = pkgs.unstable.zed-editor;
+    extensions = [
+      "nix"
+      "toml"
+      "git-firefly"
+    ];
+    userKeymaps = (builtins.fromJSON keymap_json);
+    userSettings = {
+      terminal = {
+        detect_venv = "off";
+      };
+      helix_mode = true;
+
+      # languages setup
+      languages = {
+        "Python" = {
+          language_servers = [
+            "!basedpyright"
+            "pyright"
+            "ruff"
+          ];
+          formatter = {
+            external = {
+              command = "ruff";
+              arguments = [
+                "format"
+                "-"
+                "--line-length"
+                "80"
+              ];
+            };
+          };
+        };
+      };
+
+    };
   };
+
+  # enable keyring
+  services.gnome-keyring.enable = true;
 }
