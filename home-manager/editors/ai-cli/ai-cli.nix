a8b86be8a043c06772efe3f7eebe9104cdd3badc
t1end4t <tiendatarse3004@gmail.com>
2025-10-02 16:50:23 +0700

    remember login

--- home-manager/editors/ai-cli/ai-cli.nix
+++ home-manager/editors/ai-cli/ai-cli.nix
@@ -15,6 +15,9 @@ in
     nodejs
   ];
 
+  # remember login
+  services.gnome-keyring.enable = true;
+
   # create symlink for secret aider and aider example
   home.file.".aider.conf.yml.gpg" = {
     source = "${ROOT}/secrets/aider.conf.yml.gpg";
