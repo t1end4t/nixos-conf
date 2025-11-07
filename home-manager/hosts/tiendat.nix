71348c780f157e2fb175ad86e9035779be607dec
t1end4t <tiendatarse3004@gmail.com>
2025-11-07 16:46:15 +0700

    remove thing not use, just keep everything simple

--- home-manager/hosts/tiendat.nix
+++ home-manager/hosts/tiendat.nix
@@ -1,7 +1,6 @@
 {
   imports = [
     ../default.nix
-    ./tiendat/iot.nix
   ];
 
   home.username = "tiendat";
--- home-manager/hosts/tiendat/iot.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    arduino-ide # for upload iot
-    screen # check port
-    python310 # to build
-  ];
-}
