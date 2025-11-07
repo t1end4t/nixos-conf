1dfcde5a899b2c9995dd9ef5a853edfd687046a7
t1end4t <tiendatarse3004@gmail.com>
2025-11-07 16:59:24 +0700

    strict again

--- nixos/lenovo-laptop/systemd.nix
+++ nixos/lenovo-laptop/systemd.nix
@@ -13,37 +13,23 @@ in
     };
   };
 
-  # turn off computer everyday
-  # source: https://wiki.nixos.org/wiki/Systemd/timers
-  # systemd.timers."shutdown-Tue" = {
-  #   description = "daily shutdown timer";
-  #   wantedBy = [ "timers.target" ];
-  #   timerConfig = {
-  #     # source: https://man.archlinux.org/man/systemd.time.7
-  #     # force turn off before 9PM until 4AM
-  #     OnCalendar = "Tue 21..23,00..03:*:00";
-  #     Persistent = true;
-  #     Unit = "shutdown-daily.service";
-  #   };
-  # };
+  systemd.timers."shutdown-daily" = {
+    description = "daily shutdown timer";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      # source: https://man.archlinux.org/man/systemd.time.7
+      # force turn off before 8PM until 4AM
+      OnCalendar = "*-*-* 20..23,00..03:*:00";
+      Persistent = true;
+      Unit = "shutdown-daily.service";
+    };
+  };
 
-  # systemd.timers."shutdown-daily" = {
-  #   description = "daily shutdown timer";
-  #   wantedBy = [ "timers.target" ];
-  #   timerConfig = {
-  #     # source: https://man.archlinux.org/man/systemd.time.7
-  #     # force turn off before 8PM until 4AM
-  #     OnCalendar = "Mon,Wed..Sun 20..23,00..03:*:00";
-  #     Persistent = true;
-  #     Unit = "shutdown-daily.service";
-  #   };
-  # };
-
-  # systemd.services."shutdown-daily" = {
-  #   description = "force to shutdown daily";
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     ExecStart = "/run/current-system/sw/bin/systemctl poweroff";
-  #   };
-  # };
+  systemd.services."shutdown-daily" = {
+    description = "force to shutdown daily";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "/run/current-system/sw/bin/systemctl poweroff";
+    };
+  };
 }
