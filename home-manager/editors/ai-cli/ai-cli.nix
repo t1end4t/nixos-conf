cfc180a32ad16a8befb0df0430a56c77fd70c4dd
t1end4t <tiendatarse3004@gmail.com>
2025-11-24 08:32:42 +0700

    try some file for vibe coding

--- home-manager/editors/ai-cli/ai-cli.nix
+++ home-manager/editors/ai-cli/ai-cli.nix
@@ -5,6 +5,13 @@ in
 {
   home.packages = with pkgs; [
     unstable.aider-chat-with-playwright # will replace by hinty soon
+
+    # to install gemini-cli and claude-code
+    # npm set prefix ~/.npm-global
+    # npm install -g @musistudio/claude-code-router
+    # npm install -g @anthropic-ai/claude-code
+    # npm install -g @google/gemini-cli
+    nodejs
   ];
 
   # remember login
@@ -13,5 +20,9 @@ in
   # create symlink for secret aider and aider example
   home.file.".aider.conf.yml.gpg" = {
     source = "${ROOT}/secrets/aider.conf.yml.gpg";
+  };
+
+  home.file.".claude-code-router/config.json.gpg" = {
+    source = "${ROOT}/secrets/claude-code-router-config.json.gpg";
   };
 }
