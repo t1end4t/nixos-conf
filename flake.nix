7c1ee46b1700e4e67c731ff67210bece50aed414
t1end4t <tiendatarse3004@gmail.com>
2026-01-11 14:55:47 +0700

    refactor: Use stdenv.hostPlatform.system in flake.nix outputs

--- flake.nix
+++ flake.nix
@@ -98,10 +98,12 @@
     {
       # Your custom packages
       # Accessible through 'nix build', 'nix shell', etc
-      packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      packages = forAllSystems (
+        system: import ./pkgs nixpkgs.legacyPackages.${stdenv.hostPlatform.system}
+      );
       # Formatter for your nix files, available through 'nix fmt'
       # Other options beside 'alejandra' include 'nixpkgs-fmt'
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${stdenv.hostPlatform.system}.alejandra);
 
       # Your custom packages and modifications, exported as overlays
       overlays = import ./overlays { inherit inputs; };
