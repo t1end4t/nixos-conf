74eb8d97303fccdb76ae67812b19384f4f05871d
t1end4t <tiendatarse3004@gmail.com>
2026-03-25 07:31:51 +0700

    update

--- home-manager/gui/hyprland/laptop.nix
+++ home-manager/gui/hyprland/laptop.nix
@@ -5,11 +5,12 @@
   wayland.windowManager.hyprland.settings = {
     monitor = [
       "HDMI-A-1,1360x768@60,0x0,1"
-      "eDP-1,1360x768@60,1360x0,1"
+      "eDP-1,1360x768@60,auto-right,1"
+      ",preferred,auto,1"
     ];
     workspace = [
-      "1, monitor:HDMI-A-1"
-      "2, monitor:eDP-1"
+      "1, monitor:eDP-1, default:true"
+      "2, monitor:HDMI-A-1, default:true"
     ];
   };
 }
--- home-manager/tools/git.nix
+++ home-manager/tools/git.nix
@@ -4,7 +4,11 @@
     settings.user = {
       name = "t1end4t";
       email = "tiendatarse3004@gmail.com";
+
+      # git pull apply for submodule
+      submodule.recurse = true;
     };
     lfs.enable = true;
+
   };
 }
