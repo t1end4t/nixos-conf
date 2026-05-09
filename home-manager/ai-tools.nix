989b4cc0bae622742462e25250d83204921360ce
t1end4t <tiendatarse3004@gmail.com>
2026-05-09 09:36:33 +0700

    not use several packages, i think it shoul only in second-brain

--- home-manager/ai-tools.nix
+++ home-manager/ai-tools.nix
@@ -44,35 +44,33 @@ let
   );
 in
 {
-  home.packages =
-    with pkgs;
-    [
-      ############ claude-code ############
-      # curl -fsSL https://claude.ai/install.sh | bash
-      # curl -fsSL https://opencode.ai/install | bash
-      # npm install -g @anthropic-ai/sandbox-runtime
+  home.packages = with pkgs; [
+    ############ claude-code ############
+    # curl -fsSL https://claude.ai/install.sh | bash
+    # curl -fsSL https://opencode.ai/install | bash
+    # npm install -g @anthropic-ai/sandbox-runtime
 
-      ############ router ############
-      # npm set prefix ~/.npm-global
-      # npm install -g omniroute
-      nodejs
+    ############ router ############
+    # npm set prefix ~/.npm-global
+    # npm install -g omniroute
+    nodejs
 
-      # uvx --from git+https://github.com/oraios/serena serena
-      skillPython
-      uv
+    # uvx --from git+https://github.com/oraios/serena serena
+    # skillPython
+    uv
 
-      ############ codex skill runtimes ############
-      # libreoffice-qt
-      pandoc
-      tectonic
-      imagemagick
-    ]
-    ++ lib.optionals (pkgs.nodePackages ? pptxgenjs) [
-      pkgs.nodePackages.pptxgenjs
-    ]
-    ++ lib.optionals (pkgs.nodePackages ? docx) [
-      pkgs.nodePackages.docx
-    ];
+    ############ codex skill runtimes ############
+    # libreoffice-qt
+    # pandoc
+    # tectonic
+    # imagemagick
+    # ]
+    # ++ lib.optionals (pkgs.nodePackages ? pptxgenjs) [
+    #   pkgs.nodePackages.pptxgenjs
+    # ]
+    # ++ lib.optionals (pkgs.nodePackages ? docx) [
+    #   pkgs.nodePackages.docx
+  ];
 
   # remember login
   services.gnome-keyring.enable = true;
