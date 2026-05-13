57b8706b9fe47fe8e9ec12d39c8ae5cea758daa9
t1end4t <tiendatarse3004@gmail.com>
2026-05-13 09:40:33 +0700

    enable daily shutdown timer on laptop and workstation
    - uncomment and enable systemd shutdown timer/service on laptop
    - uncomment and enable systemd shutdown timer/service on workstation
    - remove comment prefix from time-based DNS rule in blocked-names.txt

--- nixos/workstation/systemd.nix
+++ nixos/workstation/systemd.nix
@@ -1,6 +1,6 @@
 {
-  # source: https://nixos.wiki/wiki/Sway#Systemd_services
-  # kanshi systemd service
+  # # source: https://nixos.wiki/wiki/Sway#Systemd_services
+  # # kanshi systemd service
   # systemd.user.services.kanshi = {
   #   description = "kanshi daemon for multiple monitor layout";
   #   serviceConfig = {
@@ -10,24 +10,24 @@
   # };
 
   # turn off computer everyday
-  # # source: https://wiki.nixos.org/wiki/Systemd/timers
-  # systemd.timers."shutdown-daily" = {
-  #   description = "daily shutdown timer";
-  #   wantedBy = [ "timers.target" ];
-  #   timerConfig = {
-  #     # source: https://man.archlinux.org/man/systemd.time.7
-  #     # force turn off before 6PM until 6AM
-  #     OnCalendar = "*-*-* 18..23,00..05:*:00";
-  #     Persistent = true;
-  #     Unit = "shutdown-daily.service";
-  #   };
-  # };
-  # systemd.services."shutdown-daily" = {
-  #   description = "force to shutdown daily";
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     ExecStart = "/run/current-system/sw/bin/systemctl poweroff";
-  #   };
-  # };
+  # source: https://wiki.nixos.org/wiki/Systemd/timers
+  systemd.timers."shutdown-daily" = {
+    description = "daily shutdown timer";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      # source: https://man.archlinux.org/man/systemd.time.7
+      # force turn off before 6PM until 6AM
+      OnCalendar = "*-*-* 18..23,00..05:*:00";
+      Persistent = true;
+      Unit = "shutdown-daily.service";
+    };
+  };
+  systemd.services."shutdown-daily" = {
+    description = "force to shutdown daily";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "/run/current-system/sw/bin/systemctl poweroff";
+    };
+  };
 
 }
