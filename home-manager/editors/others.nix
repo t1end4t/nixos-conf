fd82104c411862d0bf5f8342017ca7115f2dc2dd
t1end4t <tiendatarse3004@gmail.com>
2026-06-25 15:43:42 +0700

    add vscode (to use ssh remote)

--- home-manager/editors/others.nix
+++ home-manager/editors/others.nix
@@ -1,3 +1,15 @@
-{ ... }: {
+{ pkgs, ... }: {
   # Add VS Code and other editor configs here
+  programs.vscode = {
+    enable = true;
+    profiles.default.extensions = with pkgs.vscode-extensions; [
+      ms-vscode-remote.remote-ssh
+      ms-python.python
+
+      anthropic.claude-code
+      openai.chatgpt
+      sst-dev.opencode
+    ];
+  };
+
 }
