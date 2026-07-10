90074f771a06f651f5bbddf1737a3d9d90e52471
t1end4t <tiendatarse3004@gmail.com>
2026-07-10 15:59:00 +0700

    update nix-ld for only run zed

--- nixos/base/daemon/nix-ld.nix
+++ nixos/base/daemon/nix-ld.nix
@@ -1,22 +1,9 @@
 { pkgs, ... }:
 {
   programs.nix-ld.enable = true;
-
   programs.nix-ld.libraries = with pkgs; [
-    stdenv.cc.cc
-    zlib
-    openssl
     alsa-lib
     wayland
-    libxkbcommon
     vulkan-loader
-    libGL
-    freetype
-    fontconfig
-    libX11
-    libXcursor
-    libXrandr
-    libXi
-    dbus
   ];
 }
