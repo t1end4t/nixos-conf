8e79aa369f4e83f247898eabf598362e8738b9dc
t1end4t <tiendatarse3004@gmail.com>
2026-07-20 10:10:17 +0700

    strict again

--- nixos/laptop/systemd.nix
+++ nixos/laptop/systemd.nix
@@ -1,24 +1,22 @@
 {
-
-  # # turn off computer everyday
-  # # source: https://wiki.nixos.org/wiki/Systemd/timers
-  # systemd.timers."shutdown-daily" = {
-  #   description = "daily shutdown timer";
-  #   wantedBy = [ "timers.target" ];
-  #   timerConfig = {
-  #     # source: https://man.archlinux.org/man/systemd.time.7
-  #     # force turn off before 8PM until 4AM
-  #     OnCalendar = "*-*-* 20,00..03:*:00";
-  #     Persistent = false;
-  #     Unit = "shutdown-daily.service";
-  #   };
-  # };
-  # systemd.services."shutdown-daily" = {
-  #   description = "force to shutdown daily";
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     ExecStart = "/run/current-system/sw/bin/systemctl poweroff";
-  #   };
-  # };
-
+  # turn off computer everyday
+  # source: https://wiki.nixos.org/wiki/Systemd/timers
+  systemd.timers."shutdown-daily" = {
+    description = "daily shutdown timer";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      # source: https://man.archlinux.org/man/systemd.time.7
+      # force turn off before 8PM until 4AM
+      OnCalendar = "*-*-* 20,00..03:*:00";
+      Persistent = false;
+      Unit = "shutdown-daily.service";
+    };
+  };
+  systemd.services."shutdown-daily" = {
+    description = "force to shutdown daily";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "/run/current-system/sw/bin/systemctl poweroff";
+    };
+  };
 }
