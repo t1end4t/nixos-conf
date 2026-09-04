f31e21560c35366be5ccc94ee85472bb065ca622
t1end4t <tiendatarse3004@gmail.com>
2026-09-04 11:11:32 +0700

    enable all extensions

--- home-manager/desktop-apps/firefox/extensions.nix
+++ home-manager/desktop-apps/firefox/extensions.nix
@@ -1,106 +1,106 @@
 {
-  # # Catppuccin-mocha-blue
-  # "{88b098c8-19be-421e-8ffa-85ddd1f3f004}" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/catppuccin-mocha-blue/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   allowed_types = "theme";
-  # };
+  # Catppuccin-mocha-blue
+  "{88b098c8-19be-421e-8ffa-85ddd1f3f004}" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/catppuccin-mocha-blue/latest.xpi";
+    installation_mode = "force_installed";
+    allowed_types = "theme";
+  };
 
-  # # Dark reader
-  # "addon@darkreader.org" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "navbar";
-  # };
+  # Dark reader
+  "addon@darkreader.org" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "navbar";
+  };
 
-  # # Browsec VPN - Free VPN for Firefox
-  # "browsec@browsec.com" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/browsec/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "navbar";
-  # };
+  # Browsec VPN - Free VPN for Firefox
+  "browsec@browsec.com" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/browsec/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "navbar";
+  };
 
-  # # DuckDuckGo
-  # "jid1-ZAdIEUB7XOzOJw@jetpack" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "menupanel";
-  # };
+  # DuckDuckGo
+  "jid1-ZAdIEUB7XOzOJw@jetpack" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "menupanel";
+  };
 
-  # # Bitwarden - Free Password Manager
-  # "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "navbar";
-  # };
+  # Bitwarden - Free Password Manager
+  "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "navbar";
+  };
 
-  # # Auto Tab Discard
-  # "{c2c003ee-bd69-42a2-b0e9-6f34222cb046}" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/auto-tab-discard/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "menupanel";
-  # };
+  # Auto Tab Discard
+  "{c2c003ee-bd69-42a2-b0e9-6f34222cb046}" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/auto-tab-discard/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "menupanel";
+  };
 
-  # # alternative for Vimium
-  # "tridactyl.vim@cmcaine.co.uk" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/tridactyl-vim/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "menupanel";
-  # };
+  # alternative for Vimium
+  "tridactyl.vim@cmcaine.co.uk" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/tridactyl-vim/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "menupanel";
+  };
 
-  # # Firefox Relay
-  # "private-relay@firefox.com" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/private-relay/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "navbar";
-  # };
+  # Firefox Relay
+  "private-relay@firefox.com" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/private-relay/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "navbar";
+  };
 
-  # # Hide youtube recommend, shorts in youtube
-  # "myallychou@gmail.com" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/youtube-recommended-videos/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "menupanel";
-  # };
+  # Hide youtube recommend, shorts in youtube
+  "myallychou@gmail.com" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/youtube-recommended-videos/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "menupanel";
+  };
 
-  # # alternative for simple tab groups
-  # "tab-stash@condordes.net" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/tab-stash/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "menupanel";
-  # };
+  # alternative for simple tab groups
+  "tab-stash@condordes.net" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/tab-stash/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "menupanel";
+  };
 
-  # # Refined GitHub
-  # "{a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad}" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/refined-github-/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "menupanel";
-  # };
+  # Refined GitHub
+  "{a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad}" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/refined-github-/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "menupanel";
+  };
 
-  # # uBlock Origin
-  # "uBlock0@raymondhill.net" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "menupanel";
-  # };
+  # uBlock Origin
+  "uBlock0@raymondhill.net" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "menupanel";
+  };
 
-  # # Privacy Badger
-  # "jid1-MnnxcxisBPnSXQ@jetpack" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "menupanel";
-  # };
+  # Privacy Badger
+  "jid1-MnnxcxisBPnSXQ@jetpack" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "menupanel";
+  };
 
-  # # Ghostery Tracker Ad Blocker - Privacy AdBlock
-  # "firefox@ghostery.com" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/ghostery/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "menupanel";
-  # };
+  # Ghostery Tracker Ad Blocker - Privacy AdBlock
+  "firefox@ghostery.com" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/ghostery/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "menupanel";
+  };
 
-  # # Disconnect
-  # "2.0@disconnect.me" = {
-  #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/disconnect/latest.xpi";
-  #   installation_mode = "force_installed";
-  #   default_area = "menupanel";
-  # };
+  # Disconnect
+  "2.0@disconnect.me" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/disconnect/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "menupanel";
+  };
 }
