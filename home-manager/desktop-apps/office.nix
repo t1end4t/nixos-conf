f86793270c3452a3ca2ae59e5f50f756b91fe0ea
t1end4t <tiendatarse3004@gmail.com>
2026-09-05 10:40:17 +0700

    apply update

--- home-manager/desktop-apps/office.nix
+++ home-manager/desktop-apps/office.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    libreoffice-qt # office tools
+    libreoffice-qt
     koodo-reader # epub reader
     obsidian # second brain
     # inkscape # vector image for papers
